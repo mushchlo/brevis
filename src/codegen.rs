5e95c72d028d533a3b20b2aca39cae9c90092365
mushchlo <derpyop29@gmail.com>
2021-10-01 23:45:34 -0500

    made gnu C itoa implementation work with itoa(0)

--- src/codegen.rs
+++ src/codegen.rs
@@ -69,6 +69,10 @@ _itoa(long long int val)
 	static char buf[32] = {0};
 	int i = 30;
 
+    if(val == 0){
+        strcpy(buf, "0");
+        return buf;
+    }
 	for(; val && i; --i, val /= 10)
 		buf[i] = "0123456789"[val % 10];
 
